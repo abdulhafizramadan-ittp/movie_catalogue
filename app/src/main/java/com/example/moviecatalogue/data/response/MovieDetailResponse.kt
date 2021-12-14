fd6f8daa2ef4bc6f3942d524639e6d8e03a5eeab
Abdul Hafiz Ramadan <20104069@ittelkom-pwt.ac.id>
2021-12-14 21:23:30 +0700

    | Create response and domain api

---
+++ app/src/main/java/com/example/moviecatalogue/data/response/MovieDetailResponse.kt
@@ -0,0 +1,59 @@
+package com.example.moviecatalogue.data.response
+
+import com.example.moviecatalogue.data.domain.MovieDetail
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetailResponse(
+
+	@field:SerializedName("backdrop_path")
+	val backdropPath: String? = null,
+
+	@field:SerializedName("overview")
+	val overview: String? = null,
+
+	@field:SerializedName("original_language")
+	val originalLanguage: String? = null,
+
+	@field:SerializedName("release_date")
+	val releaseDate: String? = null,
+
+	@field:SerializedName("vote_average")
+	val voteAverage: Double? = null,
+
+	@field:SerializedName("runtime")
+	val runtime: Int? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("title")
+	val title: String? = null,
+
+	@field:SerializedName("tagline")
+	val tagline: String? = null,
+
+	@field:SerializedName("poster_path")
+	val posterPath: String? = null,
+
+	@field:SerializedName("homepage")
+	val homepage: String? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
+
+fun MovieDetailResponse.toDomain(): MovieDetail =
+	MovieDetail(
+		backdropPath = backdropPath ?: "",
+		overview = overview ?: "",
+		originalLanguage = originalLanguage ?: "",
+		releaseDate = releaseDate ?: "",
+		voteAverage = voteAverage ?: 0.0,
+		runtime = runtime ?: 0,
+		id = id ?: 0,
+		title = title ?: "",
+		tagline = tagline ?: "",
+		posterPath = posterPath ?: "",
+		homepage = homepage ?: "",
+		status = status ?: ""
+	)
---
+++ app/src/main/java/com/example/moviecatalogue/data/response/TvShowDetailResponse.kt
@@ -0,0 +1,71 @@
+package com.example.moviecatalogue.data.response
+
+import com.example.moviecatalogue.data.domain.TvShowDetail
+import com.google.gson.annotations.SerializedName
+
+data class TvShowDetailResponse(
+
+	@field:SerializedName("first_air_date")
+	val firstAirDate: String? = null,
+
+	@field:SerializedName("overview")
+	val overview: String? = null,
+
+	@field:SerializedName("original_language")
+	val originalLanguage: String? = null,
+
+	@field:SerializedName("number_of_episodes")
+	val numberOfEpisodes: Int? = null,
+
+	@field:SerializedName("type")
+	val type: String? = null,
+
+	@field:SerializedName("poster_path")
+	val posterPath: String? = null,
+
+	@field:SerializedName("backdrop_path")
+	val backdropPath: String? = null,
+
+	@field:SerializedName("vote_average")
+	val voteAverage: Double? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("tagline")
+	val tagline: String? = null,
+
+	@field:SerializedName("id")
+	val id: Int? = null,
+
+	@field:SerializedName("number_of_seasons")
+	val numberOfSeasons: Int? = null,
+
+	@field:SerializedName("last_air_date")
+	val lastAirDate: String? = null,
+
+	@field:SerializedName("homepage")
+	val homepage: String? = null,
+
+	@field:SerializedName("status")
+	val status: String? = null
+)
+
+fun TvShowDetailResponse.toDomain(): TvShowDetail =
+	TvShowDetail(
+		firstAirDate = firstAirDate ?: "",
+		overview = overview ?: "",
+		originalLanguage = originalLanguage ?: "",
+		numberOfEpisodes = numberOfEpisodes ?: 0,
+		type = type ?: "",
+		posterPath = posterPath ?: "",
+		backdropPath = backdropPath ?: "",
+		voteAverage = voteAverage ?: 0.0,
+		name = name ?: "",
+		tagline = tagline ?: "",
+		id = id ?: 0,
+		numberOfSeasons = numberOfSeasons ?: 0,
+		lastAirDate = lastAirDate ?: "",
+		homepage = homepage ?: "",
+		status = status ?: ""
+	)
