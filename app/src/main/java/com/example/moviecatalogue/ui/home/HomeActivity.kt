a69281e5f91d6106e0fc3869ac5ece493ece4394
Abdul Hafiz Ramadan <20104069@ittelkom-pwt.ac.id>
2021-12-15 07:05:21 +0700

    | Layouting activities and fragments

--- app/src/main/java/com/example/moviecatalogue/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/moviecatalogue/ui/home/HomeActivity.kt
@@ -2,12 +2,34 @@ package com.example.moviecatalogue.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
 import com.example.moviecatalogue.R
+import com.example.moviecatalogue.databinding.ActivityHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = sectionsPagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = getString(TAB_TITLES[position])
+        }.attach()
+
+    }
+
+    companion object {
+        @StringRes
+        val TAB_TITLES = intArrayOf(
+            R.string.movie,
+            R.string.tv_show
+        )
     }
 }
