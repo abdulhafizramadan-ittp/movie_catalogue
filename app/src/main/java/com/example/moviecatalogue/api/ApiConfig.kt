4f59136d7bd62b611024c31597c8483b4b167a33
Abdul Hafiz Ramadan <abdulhafizr>
2022-01-13 17:50:24 +0700

    | Refactor di

--- app/src/main/java/com/example/moviecatalogue/api/ApiConfig.kt
+++ app/src/main/java/com/example/moviecatalogue/api/ApiConfig.kt
@@ -1,39 +1,9 @@
 package com.example.moviecatalogue.api
 
-import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerCollector
-import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.example.moviecatalogue.BuildConfig
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiConfig {
-
-    fun getInstance(context: Context): ApiService {
-        val chuckerInterceptor = OkHttpClient.Builder()
-            .addInterceptor(
-                ChuckerInterceptor.Builder(context)
-                    .collector(ChuckerCollector(context))
-                    .maxContentLength(250000L)
-                    .redactHeaders(emptySet())
-                    .alwaysReadResponseBody(false)
-                    .build()
-            )
-            .build()
-
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(chuckerInterceptor)
-            .build()
-            .create(ApiService::class.java)
-    }
-
-    companion object {
-        const val TMDB_TOKEN = BuildConfig.TMDB_TOKEN
-        const val POSTER_MD = "http://image.tmdb.org/t/p/w500"
-
-        private const val BASE_URL = "https://api.themoviedb.org/3/"
-    }
+object ApiConfig {
+    const val TMDB_TOKEN = BuildConfig.TMDB_TOKEN
+    const val POSTER_MD = "http://image.tmdb.org/t/p/w500"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
 }
--- app/src/main/java/com/example/moviecatalogue/di/AppModule.kt
+++ app/src/main/java/com/example/moviecatalogue/di/AppModule.kt
@@ -1,17 +1,43 @@
 package com.example.moviecatalogue.di
 
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.example.moviecatalogue.api.ApiConfig
+import com.example.moviecatalogue.api.ApiService
 import com.example.moviecatalogue.data.MovieRepository
 import com.example.moviecatalogue.data.remote.RemoteDataSource
 import com.example.moviecatalogue.ui.detail.movie.MovieDetailViewModel
 import com.example.moviecatalogue.ui.detail.tvShow.TvShowDetailViewModel
 import com.example.moviecatalogue.ui.movie.MovieViewModel
 import com.example.moviecatalogue.ui.tvShow.TvShowViewModel
+import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.scope.get
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
-    single { ApiConfig().getInstance(get()) }
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(
+                ChuckerInterceptor.Builder(get())
+                    .collector(ChuckerCollector(get()))
+                    .maxContentLength(250000L)
+                    .redactHeaders(emptySet())
+                    .alwaysReadResponseBody(false)
+                    .build()
+            )
+            .build()
+    }
+    single {
+        Retrofit.Builder()
+            .baseUrl(ApiConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+            .create(ApiService::class.java)
+    }
     single { RemoteDataSource(get()) }
     single { MovieRepository(get()) }
 
